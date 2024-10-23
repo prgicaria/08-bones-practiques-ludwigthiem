3bab90e65166d8718de76c34d6e117147977f10d
ludwigthiem <lthiem@instituticaria.cat>
2024-10-23 14:17:43 +0200

    Enviament 2

--- bones_practiques.py
+++ bones_practiques.py
@@ -5,18 +5,18 @@
 Institut Icària
 Programació - 1r Batxillerat - Curs 20234-24
 
-Demana als usuaris dos nombres enters, en fa la divisió i mostra per pantalla la divisió, el quocient i el residu.
+Demana als usuaris dos nombres enters, en fa la divisió i mostra per pantalla
+la divisió, el quocient i el residu.
 '''
 
-
-__author__ ="Ludwig Thiem González"
-__email__ ="lthiem@instituticaria.cat"
-__date__ ="2024/10/23"
+__author__ = "Ludwig Thiem González"
+__email__ = "lthiem@instituticaria.cat"
+__date__ = "2024/10/23"
 
 dividend = int(input("Introdueix el dividend: "))
-divisor=int(input("Introdueix el divisor: "))
-quocient=dividend // divisor
-residu=dividend % divisor
+divisor = int(input("Introdueix el divisor: "))
+quocient = dividend // divisor
+residu = dividend % divisor
 print(f"Divisó: {dividend}/{divisor}")
 print(f"Quocient: {quocient}")
 print(f"Residu: {residu}")
